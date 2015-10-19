f1633600400f0745a666eb7ef1662ca01f698497
Johannes Reuter <flash1293@gmail.com>
2015-10-19 07:14:14 +0200

    removed error-handling-stuff

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 module.exports = {
   logger: require('./lib/logger'),
   validatorFlow: require('./lib/validatorFlow'),
-  errorFactory: require('./lib/errorFactory'),
-  uncaughtExceptionHandler: require('./lib/uncaughtExceptionHandler'),
   jsonValidator: require('./lib/jsonValidator'),
-  errorHandler: require('./lib/errorHandler')
 };
